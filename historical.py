9cec8b6941b9e48193e9f9830af8277a5463bc82
mcmikels <32366885+mcmikels@users.noreply.github.com>
2017-11-01 10:09:46 -0500

    Add files via upload

---
+++ historical.py
@@ -0,0 +1,66 @@
+#Historical Analysis
+import csv
+import numpy as np
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+
+#get historical data
+with open('historical.csv','rU') as csvfile:
+    reader = csv.reader(csvfile, delimiter= ",")
+    points_for = []
+    points_against = []
+    wins = []
+    losses = []
+    deviation = []
+    for row in reader:
+        win = row[0]
+        loss = row[1]
+        PF = row[2]
+        PA = row[3]
+
+        points_for.append(PF)
+        points_against.append(PA)
+        wins.append(win)
+        losses.append(loss)
+
+        #convert strings to floats or ints
+        points_for = map(float, points_for)
+        points_against = map(float, points_against)
+        wins = map(float, wins)
+        losses = map(float, losses)
+
+#Calculate win percentage
+win_per = []
+for win,loss in zip(wins,losses):
+    win_percent = win/(win + loss)
+    win_per.append(win_percent)
+
+#Define x and y data
+x_data = []
+for pf,pa in zip(points_for,points_against):
+    x_point = pa/pf
+    x_data.append(x_point)
+
+y_data = []
+for game in win_per:
+    if game > 0:
+        y_point = (game ** -1) - 1
+    if game == 0:
+        y_point = 0.5
+    y_data.append(y_point)
+
+#fit function
+def fit(x,exp):
+    return x ** exp
+
+popt, pcov = curve_fit(fit, x_data, y_data, p0=3)
+
+print popt, pcov
+
+x_plot = np.linspace(0.6, 1.8, len(points_for))
+
+plt.scatter(x_data, y_data, label='data')
+plt.plot(x_plot, fit(x_plot, *popt), "-", label=popt)
+plt.plot(x_plot, fit(x_plot, 5.729), "-", label=5.729)
+plt.legend()
+plt.show()
